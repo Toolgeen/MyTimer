0bcc978f14f05e37bc7b31332c934c3e639464dc
Tol <htemohpab@gmail.com>
2022-06-20 13:22:37 +0600

    list fragment

--- app/src/main/java/com/abdykadyr/mytimer/presentation/MainActivity.kt
+++ app/src/main/java/com/abdykadyr/mytimer/presentation/MainActivity.kt
@@ -1,11 +1,15 @@
-package com.abdykadyr.mytimer
+package com.abdykadyr.mytimer.presentation
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import com.abdykadyr.mytimer.R
+
+
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
     }
+
 }
---
+++ app/src/main/java/com/abdykadyr/mytimer/presentation/MyTimerListFragment.kt
@@ -0,0 +1,9 @@
+package com.abdykadyr.mytimer.presentation
+
+import androidx.fragment.app.Fragment
+
+class MyTimerListFragment : Fragment() {
+
+
+
+}
