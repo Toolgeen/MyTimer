39843b1be6860a6d2c7622f485c1a0002e399dc1
Tol <htemohpab@gmail.com>
2022-06-20 16:20:45 +0600

    viewModel : RepositoryImpl & useCases 2

--- app/src/main/java/com/abdykadyr/mytimer/presentation/ApplicationViewModel.kt
+++ app/src/main/java/com/abdykadyr/mytimer/presentation/ApplicationViewModel.kt
@@ -2,13 +2,23 @@ package com.abdykadyr.mytimer.presentation
 
 import android.app.Application
 import androidx.lifecycle.AndroidViewModel
+import androidx.lifecycle.LiveData
+import androidx.lifecycle.MutableLiveData
 import com.abdykadyr.mytimer.data.RepositoryImpl
 import com.abdykadyr.mytimer.domain.*
 
 
-class ApplicationViewModel(application: Application): AndroidViewModel(application) {
+class ApplicationViewModel(application: Application) : AndroidViewModel(application) {
 
     private val repository = RepositoryImpl(application)
+
+    private var _isHoursValid = MutableLiveData(false)
+    val isHoursValid: LiveData<Boolean>
+    get() = _isHoursValid
+
+    private var _isMinutesValid = MutableLiveData(false)
+    val isMinutesValid: LiveData<Boolean>
+        get() = _isMinutesValid
 
     private val addTimerUseCase = AddTimerUseCase(repository)
     private val deleteTimerUseCase = DeleteTimerUseCase(repository)
@@ -16,6 +26,19 @@ class ApplicationViewModel(application: Application): AndroidViewModel(applicati
     private val getAllTimersUseCase = GetAllTimersUseCase(repository)
     private val getTimerUseCase = GetTimerUseCase(repository)
 
+    fun getTimers() = getAllTimersUseCase
 
+    fun addTimer(plannedTime: Int, spentTime: Int, isDone: Boolean) {
+        addTimerUseCase(
+            MyTimer(
+                plannedTime = plannedTime,
+                spentTime = spentTime,
+                isDone = isDone
+            )
+        )
+    }
 
+    fun deleteTimer(myTimerId: Int) = deleteTimerUseCase.invoke(myTimerId)
+
+    fun getTimer(myTimerId: Int) = getTimerUseCase.invoke(myTimerId)
 }
