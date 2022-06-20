f08fbee838075d8528155aa50a2cc9ce068f4481
Tol <htemohpab@gmail.com>
2022-06-20 15:47:11 +0600

    layout for add timer dialog 2

--- app/src/main/java/com/abdykadyr/mytimer/presentation/MainActivity.kt
+++ app/src/main/java/com/abdykadyr/mytimer/presentation/MainActivity.kt
@@ -2,14 +2,31 @@ package com.abdykadyr.mytimer.presentation
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.fragment.app.FragmentManager
 import com.abdykadyr.mytimer.R
-
+import com.abdykadyr.mytimer.databinding.ActivityMainBinding
 
 
 class MainActivity : AppCompatActivity() {
+
+    private lateinit var binding: ActivityMainBinding
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+        supportFragmentManager.apply {
+            popBackStack()
+            beginTransaction()
+                .replace(R.id.main_container, MyTimerListFragment.getInstance())
+                .addToBackStack(null)
+                .commit()
+        }
+
     }
+
+
 
 }
--- app/src/main/java/com/abdykadyr/mytimer/presentation/MyTimerListFragment.kt
+++ app/src/main/java/com/abdykadyr/mytimer/presentation/MyTimerListFragment.kt
@@ -1,9 +1,47 @@
 package com.abdykadyr.mytimer.presentation
 
+import android.os.Bundle
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
 import androidx.fragment.app.Fragment
+import com.abdykadyr.mytimer.R
+import com.abdykadyr.mytimer.databinding.FragmentMyTimerListBinding
+import java.lang.RuntimeException
 
 class MyTimerListFragment : Fragment() {
 
+    private var _binding: FragmentMyTimerListBinding? = null
+    val binding: FragmentMyTimerListBinding
+    get() = _binding ?: throw RuntimeException("FragmentMyTimerListBinding is null")
 
+
+
+    override fun onCreateView(
+        inflater: LayoutInflater,
+        container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View {
+        _binding = FragmentMyTimerListBinding.inflate(inflater,container,false)
+        return binding.root
+    }
+
+    override fun onDestroy() {
+        super.onDestroy()
+        _binding = null
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+
+        binding.bAddTimer.setOnClickListener {
+            AddTimerDialogFragment().show(parentFragmentManager, AddTimerDialogFragment.TAG)
+        }
+
+    }
+
+    companion object {
+        fun getInstance() = MyTimerListFragment()
+    }
 
 }
